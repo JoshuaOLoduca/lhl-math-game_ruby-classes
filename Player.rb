2364062ae6ff6ec4228fa34b574b6fab274ba4bb
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-07 12:28:23 -0500

    added loselife for player

--- Player.rb
+++ Player.rb
@@ -1,8 +1,12 @@
 class Player
-  attr_reader :name
-  attr_accessor :life
+  attr_reader :name, :life
   def initialize(name, lives = 3)
     @name = name
     @life = lives
   end
+
+  def lose_life 
+    @life -= 1
+  end
+
 end
